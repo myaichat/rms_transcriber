80bade913701fe4a2ef090eb59221a36919e8e24
alex buz <alex_buz@yahoo.com>
2024-10-28 08:29:21 -0400

    left

--- 1rms.py
+++ 1rms.py
@@ -15,7 +15,7 @@ apc.processor   = None
 apc.transcriber = None
 streamer=BiderectionalStreamer( )
 
-async def run_long_running_in_executor():
+async def run_streaming_in_executor():
     loop = asyncio.get_running_loop()
     with ThreadPoolExecutor() as executor:
         await loop.run_in_executor(executor, streamer.StartStreaming)
@@ -25,14 +25,14 @@ async def main():
     apc.trans_queue = asyncio.Queue()
     app = WxAsyncApp()  # Use WxAsyncApp for async compatibility
     #Resumable Microphone Streaming 
-    frame = RMSFrame(  title="RMS Transcribe for Google Speech", size=(400, 300))
-    frame.SetSize((1200, 1000)) 
+    frame = RMSFrame(  title="RMS Transcribe for Google Speech", size=(1200, 1000))
+    #frame.SetSize((1200, 1000)) 
     frame.Show()
     #apc.processor = AsyncProcessor(queue)
     apc.transcriber = AsyncTranscriber(apc.trans_queue)
     # Start the queue consumer task
-    #asyncio.create_task(frame.consume_queue())
-    asyncio.create_task(run_long_running_in_executor())
+
+    asyncio.create_task(run_streaming_in_executor())
 
     asyncio.create_task(frame.left_panel.tree.consume_transcription_queue())
     asyncio.create_task(frame.left_panel.tree.update_tree_periodically())
